3a166d5afd6f149d313912c96583fa4dae698bef
Ben Holmes <149116719+MrFarmerBon@users.noreply.github.com>
2025-04-05 22:36:13 +0100

    Final Push Of Day 1

--- main.cpp
+++ main.cpp
@@ -5,17 +5,17 @@
 #include <iostream>
 
 const float pi = 3.141592653589;
-const float GRAVITY_MERCURY = 3.7f * 100;
-const float GRAVITY_VENUS = 8.87f * 100;
-const float GRAVITY_EARTH = 9.810f * 100;
-const float GRAVITY_MARS = 3.71f * 100;
-const float GRAVITY_JUPITER = 24.79f * 100;
-const float GRAVITY_SATURN = 10.44f * 100;
-const float GRAVITY_URANUS = 8.69f * 100;
-const float GRAVITY_NEPTUNE = 11.15f * 100;
-const float GRAVITY_PLUTO = 0.62f * 100;
+const float GRAVITY_MERCURY = 3.7f;
+const float GRAVITY_VENUS = 8.87f;
+const float GRAVITY_EARTH = 9.81f;
+const float GRAVITY_MARS = 3.71f;
+const float GRAVITY_JUPITER = 24.79f;
+const float GRAVITY_SATURN = 10.44f;
+const float GRAVITY_URANUS = 8.69f;
+const float GRAVITY_NEPTUNE = 11.15f;
+const float GRAVITY_PLUTO = 0.62f;
 
-void drawShape(int posX,int posY, float radius, int noSides) {
+void drawShape(int posX, int posY, float radius, int noSides) {
     glBegin(GL_TRIANGLE_FAN);
 
     glVertex2f(posX, posY);
@@ -38,7 +38,7 @@ int main() {
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);  
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
     int screenX = 800;
     int screenY = 800;
     GLFWwindow* window = glfwCreateWindow(800, 800, "2D Gravity", nullptr, nullptr);
@@ -52,24 +52,33 @@ int main() {
     glfwMakeContextCurrent(window);
     glViewport(0, 0, 800, 800);
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glOrtho(0, 800, 800, 0, -1, 1); 
-    std::vector<float> Earthposition = { 400.0f,300.0f };
-    std::vector<float> Earthvelocity = { 0.0f,0.0f };
-    std::vector<float> Plutoposition = { 400.0f,300.0f };
-    std::vector<float> Plutovelocity = { 0.0f,0.0f };
+    glOrtho(0, 800, 800, 0, -1, 1);
+    std::vector<float> position = { 400.0f,100.0f };
+    std::vector<float> velocity = { 0.0f,0.0f };
 
     while (!glfwWindowShouldClose(window)) {
-        glClear(GL_COLOR_BUFFER_BIT);  
-        drawShape(1200.0f, Earthposition[1], 50.0f, 100);//EARTH BALL
-        drawShape(600.0f, Plutoposition[1], 50.0f, 100);// PLUTO BALL
-        Plutovelocity[0] += GRAVITY_PLUTO;
-        Plutoposition[1] += Plutovelocity[0];
-        Earthvelocity[0] += GRAVITY_EARTH;
-        Earthposition[1] += Earthvelocity[0];
-        glfwSwapBuffers(window);  
-        glfwPollEvents();  
-    }
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawShape(position[0], position[1], 50.0f, 100);
 
+        velocity[1] += GRAVITY_EARTH / 10.0f;
+
+        //X-AXIS ACCELERATION
+        velocity[0] += 1.0f;
+        position[0] += velocity[0];
+
+
+        position[1] += velocity[1];
+
+        if (position[1] <= 0 || position[1] >= screenY) {
+            velocity[1] = -velocity[1];
+        }
+        if (position[0] <= 0 || position[0] >= screenX) {
+            velocity[0] = -velocity[0];
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     glfwDestroyWindow(window);
     glfwTerminate();
